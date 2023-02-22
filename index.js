17b990ddebe6a51ebb491f705fb0c7f438969bef
jestrocss <efefwefefw41@gmail.com>
2023-02-23 07:39:40 +0900

    submit

---
+++ index.js
@@ -0,0 +1,15 @@
+const rollButton = document.getElementById("rollButton");
+const quoteElement = document.getElementById("adviceQuote");
+const quoteIdElement = document.getElementById("adviceId");
+
+writeAdvice();
+
+rollButton.addEventListener("click", () => writeAdvice());
+
+async function writeAdvice() {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+
+  quoteElement.innerText - data.slip.advice;
+  quoteIdElement.innerText = data.slip.id;
+}
